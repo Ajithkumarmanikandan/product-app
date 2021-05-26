8dde935e0c04b89e35b5d260d32ffeff2a46b1e4
Ajith Kumar M <ajit2608@global.chainsys.com>
2021-05-26 10:31:02 +0530

    first commit

---
+++ product.sql
@@ -0,0 +1,15 @@
+drop table product;
+create table product(id number,name varchar(200),expiry_date date);
+insert into product values(1,'Pen',DATE '2023-01-01');
+insert into product values(2,'Pencil',DATE '2023-01-01');
+insert into product values(3,'Milk',DATE '2023-01-01');
+commit;
+select * from product;
+
+echo "# product-app" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Ajithkumarmanikandan/product-app.git
+git push -u origin main
